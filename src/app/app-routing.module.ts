1e0080de1d3c2659860c770dd568354fd1549836
NelsonMilla <54814879+NelsonMilla@users.noreply.github.com>
2019-10-03 23:13:15 -0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+import { IngresarComponent } from './ingresar/ingresar.component';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { LandingComponent } from './landing/landing.component';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path:  '', redirectTo:  'home', pathMatch:  'full' },
+  { path: 'home', component: LandingComponent},
+  { path: 'registrarse', component: RegistrarseComponent  },
+  { path: 'ingresar', component: IngresarComponent}
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IngresarComponent } from './ingresar/ingresar.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresarComponent,
+    RegistrarseComponent,
+    LandingComponent,
+    NavbarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
