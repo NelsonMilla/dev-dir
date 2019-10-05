9fadfe5ed618cb29cccefe5d351722a681868a24
Antonio Cardenas <antonioalx66@gmail.com>
2019-10-05 16:25:00 -0600

    cambios de login y registro

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  mobile: string;
+  resolution;
   constructor() { }
 
   ngOnInit() {
+    this.resolution = window.innerWidth;
+    this.showmobile();
+    console.log(this.resolution);
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.resolution = window.innerWidth;
+  }
+  showmobile() {
+    if (this.resolution <= 599) {
+      this.mobile = 'mobile-menu';
+    } else {
+      this.mobile = '';
+    }
+  }
 }
